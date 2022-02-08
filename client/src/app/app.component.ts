13ba925c3abb174c6b2c8ff0c784597c4e426e64
Oskar Kulpa <73159640+GieraryHirr@users.noreply.github.com>
2022-02-08 08:51:41 +0100

    Login with web token

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
+import { AccountService } from './_services/account.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { startWith } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +13,15 @@ export class AppComponent implements OnInit {
   title = 'client';
   users: any;
 
-  constructor(private http: HttpClient)
-  {
-
-  }
+  constructor(private http: HttpClient, private accountService: AccountService) {}
   ngOnInit(): void {
     this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem("user")); //Get user from localstorage
+    this.accountService.setCurrentUser(user);
   }
 
   getUsers() {
