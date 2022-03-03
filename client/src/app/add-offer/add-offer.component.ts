f8d6126aa51db0eea37881b7da17fdc06a5eddd5
Oskar Kulpa <73159640+GieraryHirr@users.noreply.github.com>
2022-03-03 14:53:49 +0100

    Reactive form for adding offer done.

--- client/src/app/add-offer/add-offer.component.ts
+++ client/src/app/add-offer/add-offer.component.ts
@@ -1,3 +1,4 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Offer } from './../_models/offer';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -11,18 +12,39 @@ import { User } from '../_models/user';
   styleUrls: ['./add-offer.component.css']
 })
 export class AddOfferComponent implements OnInit {
-  model = new Offer(); //Must be initialize to map from html
+  addOfferForm: FormGroup;
 
   constructor(private offerService: OfferService, private toastr: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.addOfferForm = new FormGroup({
+      category: new FormControl("",Validators.required),
+      brand: new FormControl("",Validators.required),
+      model: new FormControl("",Validators.required),
+      modelYear: new FormControl("",[Validators.required, Validators.minLength(4), Validators.maxLength(4)]),
+      kilometers: new FormControl("",Validators.required),
+      engineSize: new FormControl("",Validators.required),
+      horsepowers: new FormControl("",Validators.required),
+      fuel: new FormControl("",Validators.required),
+      gearbox: new FormControl("",[Validators.required, Validators.minLength(2), Validators.maxLength(2)]),
+      color: new FormControl("",Validators.required),
+      price: new FormControl("",Validators.required),
+      post: new FormControl("",[Validators.required, Validators.minLength(4), Validators.maxLength(4)]),
+      city: new FormControl("",Validators.required),
+      description: new FormControl("",[Validators.required, Validators.maxLength(500)]),
+      appUserId: new FormControl(""),
+    })
   }
 
   addOffer() {
     const user: User = JSON.parse(localStorage.getItem("user")); //Get current user from local storage
-    this.model.appUserId = parseInt(user.id); //Set ID for offer.
+    this.addOfferForm.controls["appUserId"].setValue(parseInt(user.id)); //Set ID for offer.)
 
-    this.offerService.addOffer(this.model).subscribe(response => {
+    this.offerService.addOffer(this.addOfferForm.value).subscribe(response => {
       console.log(response);
       this.router.navigate(["/offer"]); //Go to all offers
     }), error => {
